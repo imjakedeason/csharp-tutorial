c8c6dc9bf8f85771819e1939c57044cb222d8982
Jake Deason <61044146+imjakedeason@users.noreply.github.com>
2023-08-10 19:02:01 -0700

    Drugo predavanje kraj

--- Program.cs
+++ Program.cs
@@ -1,29 +1,42 @@
 ﻿using System.Globalization;
 using System.Text.RegularExpressions;
-
-string hello = "Dobar dan.";
-string poruka = "Ovo je poruka.";
-
-Console.WriteLine(hello);
-Console.WriteLine(poruka);
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-// tip nazivVarijable = vrijednost
-// int number = 12345;
-double decimalNum = 3.14566;
-// string valuta = "€";
-Boolean trueFalse = false;
+string[] posao = new string[3] { "programer", "devops", "dizajner"};
+string[] ucenik = new string[3] { "Perica", "Ivica", "Lukica"};
+int[] ocjena = new int[3] { 2, 3, 5};
 
-int number = 12011;
-string valuta = "180";
+ucenik[1];
+ocjena[1];
 
-// casting, parsiranje
-string result = ((number + int.Parse(valuta)) / 100).ToString();
-string booleanString = trueFalse.ToString();
-string doubleString = decimalNum.ToString();
+Person user = new Person("Željko", "Zvekić", 23, true, posao[0]); //objekt (također, mora biti iznad klase ako je u istom .cs)
+Person user2 = new Person("Jake", "Deason", 24, false, posao[2]); //definiramo podatak
 
-int cijeli = (int)decimalNum;
-int cijelidva = trueFalse ? 1 : 0;
-// int result = int.Parse(Regex.Match(number, @"\d").Value, NumberFormatInfo.InvariantInfo);
+Console.WriteLine("**********************************************************");
+Console.WriteLine("Ime i prezime: {0} {1}", user.ime, user.prezime);
+Console.WriteLine("Dob: {0}", user.age);
+Console.WriteLine("Spol: {0}", user.gender ? "Muško" : "Žensko");
+Console.WriteLine("Posao: {0}", user.posao);
 
-Console.WriteLine("Cijena proizvoda je: " + cijelidva);
+Console.WriteLine("**********************************************************");
+Console.WriteLine("Ime i prezime: {0} {1}", user2.ime, user2.prezime);
+Console.WriteLine("Dob: {0}", user2.age);
+Console.WriteLine("Spol: {0}", user2.gender ? "Muško" : "Žensko");
+Console.WriteLine("Posao: {0}", user2.posao);
+Console.WriteLine("**********************************************************");
+
+class Person {
+    public string ime; //private zabranjuje korištenje gore u console.writeline
+    public string prezime;
+    public int age;
+    public bool gender; // muško true, žensko false
+    public string posao;
+
+    public Person(string ime, string prezime, int age, bool gender, string posao) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.age = age;
+        this.gender = gender;
+        this.posao = posao;
+    } 
+}
