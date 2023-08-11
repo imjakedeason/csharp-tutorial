2ea43eeea331fdd6fcd2f1f6c4a4c62d93d47a87
Jake Deason <61044146+imjakedeason@users.noreply.github.com>
2023-08-10 17:33:16 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+string hello = "Dobar dan.";
+string poruka = "Ovo je poruka.";
+
+Console.WriteLine(hello);
+Console.WriteLine(poruka);
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// tip nazivVarijable = vrijednost
+// int number = 12345;
+double decimalNum = 3.14566;
+// string valuta = "€";
+Boolean trueFalse = false;
+
+int number = 12011;
+string valuta = "180";
+
+// casting, parsiranje
+string result = ((number + int.Parse(valuta)) / 100).ToString();
+string booleanString = trueFalse.ToString();
+string doubleString = decimalNum.ToString();
+
+int cijeli = (int)decimalNum;
+int cijelidva = trueFalse ? 1 : 0;
+// int result = int.Parse(Regex.Match(number, @"\d").Value, NumberFormatInfo.InvariantInfo);
+
+Console.WriteLine("Cijena proizvoda je: " + cijelidva);
+
