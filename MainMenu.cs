6238eff69a2867572b322d70be26003e2f0a30b7
Jake Deason <61044146+imjakedeason@users.noreply.github.com>
2023-08-18 08:12:18 -0700

    A

--- MainMenu.cs
+++ MainMenu.cs
@@ -2,7 +2,7 @@ namespace Program {
     class MainMenu {
         private string zanClass;
         public string SimName() {
-            string name;
+            string? name;
             bool running = true;
             do {
                 // Ime Sima
@@ -15,12 +15,26 @@ namespace Program {
                 // Ako nema unosa, prekida se rad aplikacije
                 if (name == null || name == "") {
                     Console.WriteLine("\nGreška prilikom unosa. Aplikacija će se zatvoritit.");
-                }
-                else {
+                } else {
                     running = false;
                 }
             } while (running);
             return zanClass;
+        }
+        public int SimAge() {
+            int? age;
+            bool running = true;
+            do {
+                // Godine Sima
+                Console.WriteLine("\n******************");
+                Console.WriteLine("\nUpišite godine Sima: ");
+                age = Console.ReadLine();
+
+                // Ako nema unosa, prekida se rad aplikacije
+                if (age == null || age == "") {
+                    Console.WriteLine("\nGreška prilikom unosa. Aplikacija će se zatvoriti.");
+                } else if
+            }
         }
         public string ZanimanjeClass() {
             bool running = true;
@@ -29,25 +43,18 @@ namespace Program {
                 Console.WriteLine("1. Astronaut ");
                 Console.WriteLine("2. Doktor ");
                 Console.WriteLine("3. Kuhar ");
-                Console.WriteLine("4. Veterinar ");
-                Console.WriteLine("5. Ekonomist ");
-                Console.WriteLine("6. Cvjećar ");
-                Console.WriteLine("7. Znanstvenik ");
-                Console.WriteLine("8. Slikar ");
-                Console.WriteLine("9. Ribar ");
-                Console.WriteLine("10. Odvjetnik ");
-                Console.WriteLine("11. Barmen ");
-                Console.WriteLine("12. Trener ");
-                Console.WriteLine("13. Glumac ");
-                Console.WriteLine("14. Pjevač ");
-                Console.WriteLine("15. Učitelj ");
+                Console.WriteLine("4. Cvjećar ");
+                Console.WriteLine("5. Znanstvenik ");
+                Console.WriteLine("6. Slikar ");
+                Console.WriteLine("7. Ribar ");
+                Console.WriteLine("8. Odvjetnik ");
                 Console.WriteLine("\nUpišite broj klase zanimanja za vašeg Sima: ");
 
                 // Korisnik odabire klasu zanimanja
-                string key = Console.ReadLine();
+                string? key = Console.ReadLine();
                 int choice = key == "" ? 0 : int.Parse(key);
 
-                //Ako izbor nije 1-15, prekini rad aplikacije
+                //Ako izbor nije 1-8, prekini rad aplikacije
                 if (choice == 1) {
                     Console.WriteLine("\n******************");
                     Console.WriteLine("\nOdabrali ste klasu: Astronaut");
@@ -59,6 +66,42 @@ namespace Program {
                     Console.WriteLine("\nOdabrali ste klasu: Doktor");
                     Console.WriteLine("\n******************");
                     zanClass = "Doktor";
+                    running = false;
+                } else if (choice == 3) {
+                    Console.WriteLine("\n******************");
+                    Console.WriteLine("\nOdabrali ste klasu: Kuhar");
+                    Console.WriteLine("\n******************");
+                    zanClass = "Kuhar";
+                    running = false;
+                } else if (choice == 4) {
+                    Console.WriteLine("\n******************");
+                    Console.WriteLine("\nOdabrali ste klasu: Cvjećar");
+                    Console.WriteLine("\n******************");
+                    zanClass = "Cvjećar";
+                    running = false;
+                } else if (choice == 5) {
+                    Console.WriteLine("\n******************");
+                    Console.WriteLine("\nOdabrali ste klasu: Znanstvenik");
+                    Console.WriteLine("\n******************");
+                    zanClass = "Znanstvenik";
+                    running = false;
+                } else if (choice == 6) {
+                    Console.WriteLine("\n******************");
+                    Console.WriteLine("\nOdabrali ste klasu: Slikar");
+                    Console.WriteLine("\n******************");
+                    zanClass = "Slikar";
+                    running = false;
+                } else if (choice == 7) {
+                    Console.WriteLine("\n******************");
+                    Console.WriteLine("\nOdabrali ste klasu: Ribar");
+                    Console.WriteLine("\n******************");
+                    zanClass = "Ribar";
+                    running = false;
+                } else if (choice == 8) {
+                    Console.WriteLine("\n******************");
+                    Console.WriteLine("\nOdabrali ste klasu: Odvjetnik");
+                    Console.WriteLine("\n******************");
+                    zanClass = "Odvjetnik";
                     running = false;
                 } else {
                     Console.WriteLine("\nNedozvoljeni unos. Probajte ponovo.");
